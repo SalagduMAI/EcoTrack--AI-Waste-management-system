cb0e9f840a30118601cae7fdf00708a5f6838209
Amantha Salgadu <donovinishansalgadu@gmail.com>
2026-07-08 06:30:55 +0530

    Support dynamic uploads directory via USE_FS_BUCKET

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => env('USE_FS_BUCKET', false) ? storage_path('app/uploads') : storage_path('app/public'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
@@ -70,7 +70,7 @@ return [
     */
 
     'links' => [
-        public_path('storage') => storage_path('app/public'),
+        public_path('storage') => env('USE_FS_BUCKET', false) ? storage_path('app/uploads') : storage_path('app/public'),
     ],
 
 ];
